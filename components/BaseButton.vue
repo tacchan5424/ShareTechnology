a4aee3b5cd843860a2e33e61b8372e1ae11fef57
tacchan5424s <tacchan5424@yahoo.co.jp>
2021-09-15 18:11:30 +0900

    コンポーネントのサンプル作成

---
+++ components/BaseButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-button :type="type" :class="classes">{{ text }}</b-button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: String,
+    classes: String,
+    text: String
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ components/BaseInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-input
+    :type="type"
+    :class="classes"
+    :icon="icon"
+    :placeholder="placeholder"
+  ></b-input>
+</template>
+
+<script>
+export default {
+  props: {
+    type: String,
+    classes: String,
+    icon: String,
+    placeholder: String
+  }
+};
+</script>
+
+<style scoped></style>
