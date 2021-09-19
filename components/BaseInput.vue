ff74b1e3e64f7003cfe960dc6079619f8017f114
tacchan5424s <tacchan5424@yahoo.co.jp>
2021-09-19 09:25:30 +0900

    buefy因数トール、テキストボックスコンポーネント作成

--- components/BaseInput.vue
+++ components/BaseInput.vue
@@ -1,19 +1,29 @@
 <template>
-  <b-input
-    :type="type"
-    :class="classes"
-    :icon="icon"
-    :placeholder="placeholder"
-  ></b-input>
+  <div>
+    <section v-if="!isEdit">
+      <b-input
+        :value="value"
+        :type="type"
+        :class="classes"
+        :icon="icon"
+        :placeholder="placeholder"
+      ></b-input>
+    </section>
+    <span v-if="isEdit">
+      {{ value }}
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
   props: {
+    value: String,
     type: String,
     classes: String,
     icon: String,
-    placeholder: String
+    placeholder: String,
+    isEdit: Boolean
   }
 };
 </script>
