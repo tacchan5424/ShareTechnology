d3288dad04c7b7e1cb065004a9cf6f36ce961ada
tacchan5424s <tacchan5424@yahoo.co.jp>
2022-01-06 21:01:35 +0900

    ヘッダーにアイコンと背景色追加

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="field is-grouped is-mobile">
-    <div class="control column is-two-fifths"></div>
-    <div class="control column">
+  <div class="field is-grouped is-mobile has-background-primary">
+    <div class="control column is-3"></div>
+    <div class="control column is-3">
       <base-input
         value="test"
         :isEdit="true"
@@ -9,14 +9,35 @@
         icon="magnify"
       ></base-input>
     </div>
-    <div class="control column">
+    <div class="control column is-1">
       <base-button :func="this.test" text="検索"></base-button>
     </div>
-    <div class="control column">
-      <a href="https://google.com">お問い合わせ先</a>
-    </div>
-    <div class="control column">
-      <a href="https://yahoo.co.jp">利用規約</a>
+    <div class="control column ">
+      <b-navbar class="has-background-primary">
+        <template #start>
+          <b-navbar-item
+            class="control column is-6 has-background-primary"
+            href="https://google.com"
+          >
+            <b-icon icon="plus"></b-icon>
+            新規作成
+          </b-navbar-item>
+          <b-navbar-item
+            class="control column is-6 has-background-primary"
+            href="https://google.com"
+          >
+            <b-icon icon="email"></b-icon>
+            お問い合わせ先
+          </b-navbar-item>
+          <b-navbar-item
+            class="control column is-6 has-background-primary"
+            href="https://google.com"
+          >
+            <b-icon icon="note"></b-icon>
+            利用規約
+          </b-navbar-item>
+        </template>
+      </b-navbar>
     </div>
   </div>
 </template>
