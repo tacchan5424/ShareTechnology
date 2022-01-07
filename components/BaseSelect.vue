f53069cf86a9e1147e98bc6673250faefd6e1db7
mikiyuta <popporio9@outlook.jp>
2022-01-07 19:55:39 +0900

    問い合わせフォームの作成

---
+++ components/BaseSelect.vue
@@ -0,0 +1,54 @@
+<template>
+  <!--
+  <select v-model="selected">
+    <option v-for="option in options" v-bind:key="option.name">
+      {{ text }}
+    </option>
+  </select>
+-->
+
+  <section>
+    <b-field label="Simple">
+      <b-select placeholder="問い合わせ内容">
+        <option
+          v-for="option in options"
+          :value="option.name"
+          :key="option.name"
+        >
+          {{ option.text }}
+        </option>
+      </b-select>
+    </b-field>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    options: Array
+  }
+};
+
+/*
+new Vue({
+  el: "selected",
+  data: {
+    options: [
+      { text: "あ", name: "A" },
+      { text: "い", name: "B" },
+      { text: "う", name: "C" }
+    ]
+  }
+});
+
+const data = require("@/data/sample.json");
+
+export default {
+  data() {
+    return { data };
+  }
+};
+*/
+</script>
+
+<style scoped></style>
---
+++ pages/contact.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <div class="control column">
+      <base-select :options="options"></base-select>
+    </div>
+    <div class="control column">
+      <base-input value="test" :isEdit="true" type="textarea"></base-input>
+    </div>
+    <div class="control column">
+      <base-button :func="this.test" text="送信"></base-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheHeader from "~/components/TheHeader.vue";
+import BaseSelect from "~/components/BaseSelect.vue";
+import BaseInput from "~/components/BaseInput.vue";
+import BaseButton from "~/components/BaseButton.vue";
+
+export default {
+  components: {
+    TheHeader,
+    BaseSelect,
+    BaseInput,
+    BaseButton
+  },
+
+  data() {
+    return {
+      options: [
+        { text: "ああああああ", name: "1" },
+        { text: "いいいいいい", name: "2" },
+        { text: "うううううう", name: "3" }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped></style>
