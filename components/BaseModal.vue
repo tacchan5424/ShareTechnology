f9edf98154fbb73fc2467610881e2c57a9bc8ae3
mikiyuta <popporio9@outlook.jp>
2022-02-02 20:04:51 +0900

    Modal機能の追加

--- components/BaseModal.vue
+++ components/BaseModal.vue
@@ -1,115 +1,69 @@
 <template>
   <section>
-    <form action="">
-      <div class="modal-card is-gapless" style="width: auto">
-        <header class="modal-card-head">
-          <p class="modal-card-title">新規作成画面</p>
-          <button type="button" class="delete" @click="$emit('close')" />
-        </header>
+    <div class="modal-card is-gapless" style="width: auto">
+      <header class="modal-card-head">
+        <p class="modal-card-title">新規作成画面</p>
+        <button type="button" class="delete" @click="$emit('close')" />
+      </header>
 
-        <base-input
-          v-model="message"
-          placeholder="edit me"
-          :isEdit="true"
-          type="textbox"
-        ></base-input>
+      <base-input
+        v-model="message"
+        placeholder="edit me"
+        :isEdit="true"
+        type="textbox"
+      ></base-input>
 
-        <p style="background-color: #ffffff">{{ message }}とは</p>
-        <base-input
-          v-model="detail"
-          :isEdit="true"
-          type="textarea"
-        ></base-input>
+      <p style="background-color: #ffffff">{{ message }}とは</p>
+      <base-input v-model="detail" :isEdit="true" type="textarea"></base-input>
 
-        <b-field
-          horizontal
-          label="参考用のサイト名："
-          class="app control column is-10 control columns is-centered"
-        >
-          <base-input
-            v-model="detail"
-            :isEdit="true"
-            type="textbox"
-          ></base-input>
-        </b-field>
+      <b-field
+        horizontal
+        label="参考用のサイト名："
+        class="noMargin control column is-10 control columns is-centered"
+      >
+        <base-input v-model="detail" :isEdit="true" type="textbox"></base-input>
+      </b-field>
 
-        <b-field horizontal label="参考用URL名：" class="app">
-          <base-input
-            v-model="detail"
-            :isEdit="true"
-            type="textbox"
-          ></base-input>
-        </b-field>
+      <b-field horizontal label="参考用URL名：" class="noMargin">
+        <base-input v-model="detail" :isEdit="true" type="textbox"></base-input>
+      </b-field>
 
-        <b-field horizontal label="参考用のサイト名：" class="app">
-          <base-input
-            v-model="detail"
-            :isEdit="true"
-            type="textbox"
-          ></base-input>
-        </b-field>
+      <b-field horizontal label="参考用のサイト名：" class="noMargin">
+        <base-input v-model="detail" :isEdit="true" type="textbox"></base-input>
+      </b-field>
 
-        <b-field horizontal label="参考用URL名：" class="app">
-          <base-input
-            v-model="detail"
-            :isEdit="true"
-            type="textbox"
-          ></base-input>
-        </b-field>
+      <b-field horizontal label="参考用URL名：" class="noMargin">
+        <base-input v-model="detail" :isEdit="true" type="textbox"></base-input>
+      </b-field>
 
-        <b-field horizontal label="参考用のサイト名：" class="app">
-          <base-input
-            v-model="detail"
-            :isEdit="true"
-            type="textbox"
-          ></base-input>
-        </b-field>
+      <b-field horizontal label="参考用のサイト名：" class="noMargin">
+        <base-input v-model="detail" :isEdit="true" type="textbox"></base-input>
+      </b-field>
 
-        <b-field horizontal label="参考用URL名：" class="app">
-          <base-input
-            v-model="detail"
-            :isEdit="true"
-            type="textbox"
-          ></base-input>
-        </b-field>
+      <b-field horizontal label="参考用URL名：" class="noMargin">
+        <base-input v-model="detail" :isEdit="true" type="textbox"></base-input>
+      </b-field>
 
-        <b-field horizontal label="参考用のサイト名：" class="app">
-          <base-input
-            v-model="detail"
-            :isEdit="true"
-            type="textbox"
-          ></base-input>
-        </b-field>
+      <b-field horizontal label="参考用のサイト名：" class="noMargin">
+        <base-input v-model="detail" :isEdit="true" type="textbox"></base-input>
+      </b-field>
 
-        <b-field horizontal label="参考用URL名：" class="app">
-          <base-input
-            v-model="detail"
-            :isEdit="true"
-            type="textbox"
-          ></base-input>
-        </b-field>
+      <b-field horizontal label="参考用URL名：" class="noMargin">
+        <base-input v-model="detail" :isEdit="true" type="textbox"></base-input>
+      </b-field>
 
-        <b-field horizontal label="参考用のサイト名：" class="app">
-          <base-input
-            v-model="detail"
-            :isEdit="true"
-            type="textbox"
-          ></base-input>
-        </b-field>
+      <b-field horizontal label="参考用のサイト名：" class="noMargin">
+        <base-input v-model="detail" :isEdit="true" type="textbox"></base-input>
+      </b-field>
 
-        <b-field horizontal label="参考用URL名：" class="app">
-          <base-input
-            v-model="detail"
-            :isEdit="true"
-            type="textbox"
-          ></base-input>
-        </b-field>
+      <b-field horizontal label="参考用URL名：" class="noMargin">
+        <base-input v-model="detail" :isEdit="true" type="textbox"></base-input>
+      </b-field>
 
-        <footer class="modal-card-foot">
-          <base-button text="送信"></base-button>
-        </footer>
-      </div>
-    </form>
+      <footer class="modal-card-foot">
+        <base-button text="送信"></base-button>
+      </footer>
+    </div>
   </section>
 </template>
 
@@ -134,7 +88,7 @@ export default {
 </script>
 
 <style>
-.app {
+.noMargin {
   margin: 0px !important; /*恐らくbuefyで使用されているCSSの優先度の方が高く、上手く修正できなかったため、簡易的に最優先になる!importantを使用*/
   background-color: #ffffff;
 }
